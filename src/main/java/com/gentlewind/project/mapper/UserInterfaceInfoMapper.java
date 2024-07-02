a670d253fc8f38a5d619065cbbd59a559a714dbc
gentlewindt <2293979632qq.com>
2024-07-02 10:54:21 +0800

    实现查询调用次数最多的接口信息列表功能

--- src/main/java/com/gentlewind/project/mapper/UserInterfaceInfoMapper.java
+++ src/main/java/com/gentlewind/project/mapper/UserInterfaceInfoMapper.java
@@ -3,6 +3,8 @@ package com.gentlewind.project.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gentlewind.apicommon.model.entity.UserInterfaceInfo;
 
+import java.util.List;
+
 /**
 * @author gentlewind
 * @description 针对表【user_interface_info(用户调用接口关系表)】的数据库操作Mapper
@@ -10,6 +12,8 @@ import com.gentlewind.apicommon.model.entity.UserInterfaceInfo;
 * @Entity com.gentlewind.project.model.entity.UserInterfaceInfo
 */
 public interface UserInterfaceInfoMapper extends BaseMapper<UserInterfaceInfo> {
+
+    List<UserInterfaceInfo> listTopInvokeInterfaceInfo(int limit);
 
 }
 
